dcb28e5cb584e717ef566682901d542da9dca9b2
Vogel, Daniel <Daniel.Vogel@vogelfree.software>
2023-08-15 12:54:27 +0200

    Add common lsp server

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -2,7 +2,16 @@ require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = {
         "lua_ls",
-        "csharp_ls"
+        "csharp_ls",
+        "ansiblels",
+        "pylsp",
+        "yamlls",
+        "sqlls",
+        "terraformls",
+        "rust_analyzer",
+        "helm_ls",
+        "fsautocomplete",
+        "dockerls",
     }
 })
 
