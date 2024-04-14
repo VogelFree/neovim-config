bd139a9726c2c044a67558feb220389f22a82789
Vogel, Daniel <Daniel.Vogel@vogelfree.software>
2024-04-14 17:36:28 +0200

    Use extra instead of own config ¨

--- nvim/lua/plugins/mason.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "williamboman/mason.nvim",
-  opts = function(_, opts)
-    if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "netcoredbg", "csharpier" })
-    end
-  end,
-}
