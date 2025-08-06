4224d4094c010e1ec34988472093989dd47e4bd0
Vogel, Daniel <Daniel.Vogel@draeger.com>
2025-08-06 12:02:35 +0200

    Set colorscheme

---
+++ lua/vogeldan/lazy/rose-pine.lua
@@ -0,0 +1,18 @@
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		color = color or "rose-pine-moon"
+		vim.cmd.colorscheme(color)
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		require('rose-pine').setup({
+			disable_background = true,
+			styles = {
+				italic = false,
+			},
+		})
+
+	end
+}
